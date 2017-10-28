fcfa5ce7b9e2ffac0f405372096f69e9970976bf
Jarvist <Jarvist@126.com>
2017-10-28 10:42:10 +0800

    1028

---
+++ app/src/main/java/com/example/jarvist/minilock/launchActivity.java
@@ -0,0 +1,12 @@
+package com.example.jarvist.minilock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class launchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
